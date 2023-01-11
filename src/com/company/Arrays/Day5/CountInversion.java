76df81175bb4aa2e92c1c603f787ff5ad4af21a1
Nandini Agarwal <anandini18git@gmail.com>
2023-01-11 22:50:04 +0530

    Day 5

---
+++ src/com/company/Arrays/Day5/CountInversion.java
@@ -0,0 +1,26 @@
+package com.company.Arrays.Day5;
+
+public class CountInversion {
+    public static void main(String[] args) {
+
+        long[] arr = {2, 4, 1, 3, 5};
+        System.out.println(inversionCount(arr,5));
+    }
+
+    // TC -> O(N^2)
+    // SC -> O(1)
+    public static long inversionCount(long arr[], long N)
+    {
+        long count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(i>j && arr[i]<arr[j]) {
+                    count++;
+                    System.out.println(arr[j]+","+arr[i]);
+                }
+
+            }
+        }
+        return count;
+    }
+}
---
+++ src/com/company/Arrays/Day5/Day5.md
@@ -0,0 +1,14 @@
+# Day 5
+- [x] Best time to Buy and Sell Stocks - 3 approaches
+    - ```O(N^2))``` ```SC : O(1)```
+    - ```O(N)``` ```SC : O(N)```
+    - ```O(N)``` ```SC : O(1)```
+- [x] Next Permutation 
+    - TC : ```O(N)```
+    - SC : ```O(1)``` 
+    
+- [x] Count Inversion
+    - Brute Force
+    - TC : O(n^2)
+    - SC : O(1)
+
